1c4743afcecde0eee2260addff7a73c8fe24a0bc
Ashley <lauraashleywalker4@gmail.com>
2022-05-29 11:03:32 -0400

    enter info to models: products and category

--- Develop/models/Category.js
+++ Develop/models/Category.js
@@ -7,7 +7,22 @@ class Category extends Model {}
 Category.init(
   {
     // define columns
+    id:{
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+
+    category_name:{
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
   },
+
   {
     sequelize,
     timestamps: false,
